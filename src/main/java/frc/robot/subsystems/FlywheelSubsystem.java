f2bf8b1476044e148e8b0a6b5fa6251e0658804d
unknown <frc9157@mnstempartners.org>
2026-04-01 17:58:03 -0500

    flywheel motor redo

--- src/main/java/frc/robot/subsystems/FlywheelSubsystem.java
+++ src/main/java/frc/robot/subsystems/FlywheelSubsystem.java
@@ -6,28 +6,23 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.FlywheelConstants;
+import frc.robot.lib.TalonSMotor;
 
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.SlotConfigs;
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
-import com.ctre.phoenix6.hardware.TalonFXS;
-import com.ctre.phoenix6.signals.MotorAlignmentValue;
 
 public class FlywheelSubsystem extends SubsystemBase {
 
-    // SparkMax main;
-    TalonFXS main;
-    // SparkMax follower;
-    TalonFXS follower;
+    TalonSMotor main;
+    TalonSMotor follower;
 
     StatusSignal<AngularVelocity> velocity;
 
     public FlywheelSubsystem() {
 
 
-        this.main = new TalonFXS(FlywheelConstants.kLeftMainFlywheelCanId);
+        this.main = new TalonSMotor(FlywheelConstants.kLeftMainFlywheelCanId);
 
 
         SlotConfigs slot = new SlotConfigs();
@@ -36,38 +31,35 @@ public class FlywheelSubsystem extends SubsystemBase {
 
         MotionMagicConfigs mmConfig = Constants.FlywheelConstants.FlywheelPID.applyTalon(slot);
 
-        var config = this.main.getConfigurator();
+        var config = this.main.getInner().getConfigurator();
         
         config.apply(slot);
         config.apply(mmConfig);
 
-        this.velocity = this.main.getVelocity();
+        this.follower = new TalonSMotor(FlywheelConstants.kRightFollowFlywheelCanId);
 
-        this.follower = new TalonFXS(FlywheelConstants.kRightFollowFlywheelCanId);
-
-        this.follower.setControl(new Follower(FlywheelConstants.kLeftMainFlywheelCanId, MotorAlignmentValue.Opposed));
+        this.follower.follow(FlywheelConstants.kLeftMainFlywheelCanId, true);
 
         config.apply(slot);
         config.apply(mmConfig);
     }
 
     public void setFlywheel(double rotationsPerSecond) {
-        this.main.setControl(new MotionMagicVelocityVoltage(rotationsPerSecond));
+        this.main.setVelocity(rotationsPerSecond);
     }
 
 
     public void stopFlywheel() {
-        this.main.stopMotor();
+        this.main.stop();
     }
 
     public double getFlywheelRotationsPerSecond() {
-        this.velocity.refresh();
-        return this.velocity.getValueAsDouble();
+        return this.main.getVelocity();
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("Flywheel/RPS", this.getFlywheelRotationsPerSecond());
+        this.main.postMotorDiagnostics();
     }
 
 }
