efdfcc25d2dc3c1223eda9c8cd0b201f4ffca514
unknown <frc9157@mnstempartners.org>
2026-04-01 20:18:14 -0500

    start of sim motor

---
+++ src/main/java/frc/robot/lib/SimSparkMotor.java
@@ -0,0 +1,17 @@
+package frc.robot.lib;
+
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
+public class SimSparkMotor implements PidMotor, MotorDiag {
+    
+    SparkMaxSim motor;
+
+    public SimSparkMotor(int canId, boolean brushless) {
+
+        DCMotor m = new DCMotor()
+
+
+        this.motor = new SparkMaxSim
+    }
+
+}
